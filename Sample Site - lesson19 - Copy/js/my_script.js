03ed000a0ad80ab734f9caaa739bf228f9b0ebbf
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-11 18:54:32 +0100

    Added my_script.js to lesson 19 folder

---
+++ Sample Site - lesson19 - Copy/js/my_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("h2").removeClass("underline");
+    $("nav").removeClass("border");
+    $("h2").addClass("underline");
+});
+$(document).ready(function(){
+    $("tr:odd").css("background-color", "red");
+    $("tr:even").css("background-color", "pink");
+})
