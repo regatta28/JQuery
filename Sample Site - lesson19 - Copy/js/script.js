3266658afbc668c422eac57dad7b57d6a2ca02b6
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-12 16:46:58 +0100

    Method Chaining

--- Sample Site - lesson19 - Copy/js/script.js
+++ Sample Site - lesson19 - Copy/js/script.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
         $("#hr_html").addClass("font_increase");
     });
 
-    $("button").hover(function(){
-        $("body").addClass("bodycolor_change");
-    });
-    $("button").mouseleave(function(){
-        $("body").addClass("bodycolor_grey");
-    });
-    $("#button1").hide(500);
+    // $("button").hover(function(){
+    //     $("body").addClass("bodycolor_change");
+    // });
+    // $("button").mouseleave(function(){
+    //     $("body").addClass("bodycolor_grey");
+    // });
+    // $("#button1").hide(500);
     $("#button2").click(function(){
         $("#paragraph2").slideToggle();
         });
@@ -29,5 +29,25 @@ $(document).ready(function() {
     $("#button3").mouseleave(function(){
         $("#button3").fadeTo(1000, 1);
     });
+    $("#button1").mouseenter(function(){
+        $("#button1").removeClass("makeRed").addClass("makeBorder");
+    });
+    $("#button1").mouseleave(function(){
+        $("#button1").removeClass("makeBorder").addClass("makeRed");
+    });
+    $("#button1").click(function(){
+        $("p").slideToggle();
+    });
+    $("#button1").click(function(){
+        $("#button1").hide("slow").show("slow");
+    });
+    $("#button1").click(function(){
+        $("#button1").fadeOut("slow").fadeIn("slow");
+    });
+
+    
+   
+
+    
 
 }); 
