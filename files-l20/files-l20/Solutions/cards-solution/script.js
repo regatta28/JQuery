38879d857b775d38359248a532c7acd0ed821cdf
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-14 11:28:15 +0100

    Hide and show divs when another button is clicked using jQuery

--- files-l20/files-l20/Solutions/cards-solution/script.js
+++ files-l20/files-l20/Solutions/cards-solution/script.js
@@ -28,6 +28,12 @@ $(document).ready(function() {
 	$(".card").click(function(){
 		$(this).toggleClass("highlight");
 	});
+	$("#select").click(function(){
+		$(".card:not(.highlight)").hide();
+	});
+	$("#all").click(function(){
+		$(".card").show();
+	});
 
 
 }); 
