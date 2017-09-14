f2b4bcc94288d882f5fa4acf54cd6ddb39972dcc
Rebecca Stevens <rsbjshan@gmail.com>
2017-09-14 21:59:27 +0100

    Table.html with .siblings and .children (jQuery)

--- files-l18/js/my_script.js
+++ files-l18/js/my_script.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
     $("h2").removeClass("underline");
     $("nav").removeClass("border");
     $("h2").addClass("underline");
+
+// $(document).ready(function(){
+//     $("tr:odd").css("background-color", "red");
+//     $("tr:even").css("background-color", "pink");
+// })
+// $("p").click(function(){
+//    $("p").css("color", "red");
+// });
+
+$("th").click(function(){
+    $("tr").children().removeClass("highlight");
+    $(this).siblings().addClass("highlight");    
 });
-$(document).ready(function(){
-    $("tr:odd").css("background-color", "red");
-    $("tr:even").css("background-color", "pink");
-})
-$("p").click(function(){
-   $("p").css("color", "red");
+
 });
